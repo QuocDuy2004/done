91cfa89e6017d858cc8d5100fda159a55f707130
Pham-Quoc-Duy <duybuntv@gmail.com>
2024-04-13 15:54:06 +0700

    first commit

---
+++ app/src/main/java/com/example/quocduy/ApiChat.java
@@ -0,0 +1,2 @@
+package com.example.quocduy;public class ApiChat {
+}
---
+++ app/src/main/java/com/example/quocduy/tab_layout/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.quocduy.tab_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quocduy.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
